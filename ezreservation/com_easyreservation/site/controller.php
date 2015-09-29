3077d041068cabd6bc8f919758bc24a0f527c0e8
johannes <johannes.tigges@freenet.de>
2015-09-29 21:24:36 +0200

    first try for views

---
+++ ezreservation/com_easyreservation/site/controller.php
@@ -0,0 +1,14 @@
+<?php
+// No direct access to this file
+defined('_JEXEC') or die('Restricted access');
+ 
+// import Joomla controller library
+jimport('joomla.application.component.controller');
+ 
+/**
+ * Easy Reservation Component Controller
+ */
+class EasyReservationController extends JControllerLegacy
+{
+}
+?>
---
+++ ezreservation/com_easyreservation/site/easyreservation.php
@@ -0,0 +1,18 @@
+<?php 
+
+// No direct access to this file
+defined('_JEXEC') or die('Restricted access');
+
+// import joomla controller library
+jimport('joomla.application.component.controller');
+
+// Get an instance of the controller prefixed by EasyReservation
+$controller = JControllerLegacy::getInstance('EasyReservation');
+
+// Perform the Request task
+$input = JFactory::getApplication()->input;
+$controller->execute($input->getCmd('task'));
+
+// Redirect if set by the controller
+$controller->redirect();
+?>
